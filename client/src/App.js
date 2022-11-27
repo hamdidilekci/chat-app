f37ac6022a47dcd8851f73071673b156e5aa7df0
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-27 20:16:24 +0300

    add init function(backend connection) in useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
+import { useEffect } from 'react';
+
+import { ChatProvider } from './context/ChatContext';
+import { init } from './socketApi';
 import ChatForm from './components/ChatForm';
 import ChatList from './components/ChatList';
-import { ChatProvider } from './context/ChatContext';
+
+import './App.css';
 
 function App() {
+
+  useEffect(() => {
+
+    init();
+
+  }, []);
+
   return (
     <ChatProvider>
       <div className='App'>
-        <ChatList/>
-        <ChatForm/>
+        <ChatList />
+        <ChatForm />
       </div>
     </ChatProvider>
   );
