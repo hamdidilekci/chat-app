349c83a6f1bc12ef7d5860b4bb252e34cc9afb86
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-27 20:15:19 +0300

    make connection to backend

--- client/src/socketApi.js
+++ client/src/socketApi.js
@@ -1,1 +1,13 @@
 import io from 'socket.io-client';
+
+let socket;
+
+export const init = () => {
+    console.log('Connecting...');
+
+    socket = io('http://localhost:3001', {
+        transports: ['websocket'],
+    });
+
+    socket.on('connect', () => console.log('Connected!'));
+};
