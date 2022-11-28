648a8f29f22f97f7cfe56cfbfde94246d7dacd24
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-28 13:35:51 +0300

    add sendMessage method for display messages in chatList

--- client/src/socketApi.js
+++ client/src/socketApi.js
@@ -11,3 +11,13 @@ export const init = () => {
 
     socket.on('connect', () => console.log('Connected!'));
 };
+
+export const sendMessage = (message) => {
+    if(socket) socket.emit('new-message', message);
+};
+
+export const subscribeChat = () => {
+    if(socket) socket.on('receive-message', (message) => {
+        console.log(message)
+    })
+}
